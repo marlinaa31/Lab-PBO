add842ebcf4464bf457412eb0aaf9da95a67ca1a
Marlina <101453667+marlinaa31@users.noreply.github.com>
2022-09-10 19:37:36 +0700

    Add files via upload

---
+++ praktikum/CharAtExample.java
@@ -0,0 +1,11 @@
+public class CharAtExample
+{
+   public static void main(String arg[])
+   {
+       String name="Java is cool";
+	   char ch=name.charAt(3);
+	   
+	   // mengembalikan karakter pada indeks ke-3
+	   System.out.println(ch);
+	}
+}
